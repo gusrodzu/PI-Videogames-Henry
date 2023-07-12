cc066f7f7f62f3d6eedc143f9767682f959c87bd
Gus Rodríguez <gorz12@gmail.com>
2023-07-11 21:40:16 -0600

    components/redux

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./card.module.css";
+
+const Card = ({ name, image, genres, id }) => {
+  // let allgenres = "";
+  // if (Array.isArray(genres)) {
+  //   let genresdb = [];
+  //   genres.forEach((el) => genresdb.push(el.name));
+  //   allgenres = genresdb.join(", ");
+  // } else {
+  //   allgenres = genres;
+  // }
+
+  let allgenres = Array.isArray(genres)
+    ? genres.map((el) => el.name).join(", ")
+    : genres;
+
+  return (
+    <div className={style.card}>
+      <h4 className={style.genre}>
+
+        <ul className={style.genresList}>
+          {Array.isArray(genres) ? (
+            genres.map((el, i) => <li key={i}>{el.name}</li>)
+          ) : (
+            <li>{genres}</li>
+          )}
+        </ul>
+        
+      </h4>
+
+      <img src={image} alt={name} />
+
+      <div className={style.overlay}>
+        <div className={style.cardinfo}>
+          <Link to={`/videogame/${id}`} className={style.link}>
+            <h3 className={style.title}>{name}</h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
